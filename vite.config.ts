c4948983ff3dd67ec82ab1307f994a1b883c801d
ElnukyNika <elnuky@gmail.com>
2023-09-13 00:55:49 -0400

    Added predeploy and deploy to package.json files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+base: "/Random_Quote_Generator"
 export default defineConfig({
   plugins: [react()],
 })
